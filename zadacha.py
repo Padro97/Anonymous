84971a6e0941ff5df1e59e9bdb5e38be04af8be8
chulykalex <chulyk04@mail.ru>
2023-06-06 16:58:43 +0300

    отредактировал функцию

--- zadacha.py
+++ zadacha.py
@@ -1,8 +1,12 @@
-def convert_to_uppercase(input_string):
-result = ""
-for char in input_string:
-if 'a' <= char <= 'z':
-result += chr(ord(char) - 32)
-else:
-result += char
-return result
+def to_uppercase(input_string):
+    output_string = ""
+    for char in input_string:
+        if "a" <= char <= "z":
+            output_string += chr(ord(char) - 32)
+        else:
+            output_string += char
+    return output_string
+
+input_string = input("Введите строку: ")
+result = to_uppercase(input_string)
+print("Результат: ", result)
